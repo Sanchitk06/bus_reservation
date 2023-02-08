936bdff5bc9d1bc19707b755153868ab9e8005fa
sanchitks06 <f16@bmsit.in>
2023-02-08 21:40:26 +0530

    add

---
+++ site/avaliabe_buses/script.js
@@ -0,0 +1,5 @@
+var from = document.getElementByID(from).innerHTML;
+var dest = document.getElementByID(destination).innerHTML;
+
+document.getElementByID("presentplace").innerHTML = from;
+document.getElementByID("destination").innerHTML = dest;
